6fc9d6f48b98beaf3cf0771bf7ae855287859f9f
Kübra Moğul <kubramogul06@gmail.com>
2021-03-02 17:06:13 +0300

    Add project files.

---
+++ MyBlog/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BlogContext _context;
+        public HomeController(ILogger<HomeController> logger, BlogContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _context.Blog.Take(4).Where(b=>b.IsPublish).OrderByDescending(x=>x.CreateTime).ToList();
+            foreach(var blog in list)
+            {
+                blog.Author = _context.Author.Find(blog.AuthorId);
+               
+            }
+            return View(list);
+        }
+        public IActionResult About()
+        {
+            
+            return View();
+        }
+        public IActionResult Contact()
+        {
+
+            return View();
+        }
+
+        public IActionResult Post(int Id)
+        {
+            var blog = _context.Blog.Find(Id);
+            blog.Author = _context.Author.Find(blog.AuthorId);
+            blog.ImagePath = "/img/" + blog.ImagePath;
+            return View(blog);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
